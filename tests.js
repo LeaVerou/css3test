3caefe42871280190e6605b56c99cb9870ca6e7b
Ҽ˿ <jie.lijie@alibaba-inc.com>
2014-10-29 16:39:53 +0800

    matches 少了冒号

--- tests.js
+++ tests.js
@@ -159,7 +159,7 @@ window.Specs = {
 			":only-of-type": ":only-of-type",
 			":empty": ":empty",
 			":not()": [":not(*)", ":not(element)", ":not(.class):not(#id):not([attr]):not(:link)"],
-      ":matches()": [".foo :matches(h1, h2, h3, h4, h5, h6)", "matches(section, nav) h1", "matches(section, nav) ~ :matches(h1, h2)", ":matches(:hover, :focus)"],
+      ":matches()": [".foo :matches(h1, h2, h3, h4, h5, h6)", ":matches(section, nav) h1", ":matches(section, nav) ~ :matches(h1, h2)", ":matches(:hover, :focus)"],
       ":any()": [".foo :any(h1, h2, h3, h4, h5, h6)", "any(section, nav) h1", "any(section, nav) ~ :any(h1, h2)", ":any(:hover, :focus)"],
       ":drop()": [":drop(active)" ,":drop(valid active invalid)"],
       ":current()": ":current(p, li, dt, dd)",
