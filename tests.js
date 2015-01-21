103f3c04be352ef1ac4ab09c4c7b2083d5d23863
一丝 <jie.lijie@alibaba-inc.com>
2015-01-21 19:20:43 +0800

    sel to s

--- tests.js
+++ tests.js
@@ -256,8 +256,8 @@ window.Specs = {
             ],
 
             ":future": ":future",
-            ":nth-child(An+B [of sel]?)": [":nth-child(-n+3 of li.important)", ":nth-child(even of :not([hidden]))", "tr:nth-child(even of tr:nth-child(odd))"],
-            ":nth-last-child(An+B [of sel]?)": [":nth-last-child(-n+3 of li.important)", ":nth-last-child(even of :not([hidden]))"],
+            ":nth-child(An+B [of s]?)": [":nth-child(-n+3 of li.important)", ":nth-child(even of :not([hidden]))", "tr:nth-child(even of tr:nth-child(odd))"],
+            ":nth-last-child(An+B [of s]?)": [":nth-last-child(-n+3 of li.important)", ":nth-last-child(even of :not([hidden]))"],
             ":lang()": [":lang('*-Hant')", ":lang(zh, '*-hant')", ":lang(*-Hant-CN)", ":lang(en-)", ":lang(en--)", ":lang(en, de-DE)"],
             //https://bugs.webkit.org/show_bug.cgi?id=118162
             //https://bugzilla.mozilla.org/show_bug.cgi?id=1069012
