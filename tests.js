51b3ea882fcf734796b061e88f1f011d691bad48
一丝 <jie.lijie@alibaba-inc.com>
2015-01-21 15:15:06 +0800

    remove main-size

--- tests.js
+++ tests.js
@@ -721,7 +721,7 @@ window.Specs = {
             "flex": ["none", "auto", "0", "5 7 10%"],
             "flex-grow": ["0", "5"],
             "flex-shrink": ["1", "10"],
-            "flex-basis": ["auto", "main-size", "content", "1px"],
+            "flex-basis": ["auto", "1px", "content", "fill", "fill-available", "-moz-available", "min-content", "max-content", "fit-content"],
             "order": ["0", "1"]
         }
     },
