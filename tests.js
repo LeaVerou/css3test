d9e10c0ca6bca6adfa09d2728fe357eef10ffd00
Emil Björklund <emil.bjorklund@inuse.se>
2015-09-05 16:13:55 +0200

    Comment on test status for gap.

--- tests.js
+++ tests.js
@@ -514,6 +514,7 @@ window.Specs = {
 			"grid-column": ["auto", "1", "-1", "1 / 1", "1 / -1", "auto / auto", "2 / span 2"],
 			"grid-row": ["auto", "1", "-1", "1 / 1", "1 / -1", "auto / auto", "2 / span 2"],
 			"grid-area": ["1 / 1", "1 / span 1", "span / 10 / -1"]
+			/* These are on their way into the draft spec, currently only in editors draft */
 			// "grid-column-gap": ["normal", "1em"],
 			// "grid-row-gap": ["normal", "1em"],
 			// "grid-gap": ["normal", "normal 1em", "1em", "1em 1em"]
