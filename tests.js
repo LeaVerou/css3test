77b159fc00d8b346bc07dabf9cd185bb95279dd6
Zéfling (Célian) <zefling@ikilote.net>
2019-08-10 22:44:55 +0200

    Update Image Values Level 3 & 4

--- tests.js
+++ tests.js
@@ -67,7 +67,7 @@ window.Specs = {
 				"cursor",
 				"content"
 			],
-			"linear-gradient": [
+			"linear-gradient()": [
 				"linear-gradient(white, black)",
 				"linear-gradient(to right, white, black)",
 				"linear-gradient(45deg, white, black)",
@@ -77,7 +77,7 @@ window.Specs = {
 				"linear-gradient(currentColor, black)",
 				"linear-gradient(red -50px, white calc(-25px + 50%), blue 100%)"
 			],
-			"radial-gradient": [
+			"radial-gradient()": [
 				"radial-gradient(white, black)",
 				"radial-gradient(circle, white, black)",
 				"radial-gradient(ellipse, white, black)",
@@ -92,13 +92,8 @@ window.Specs = {
 				"radial-gradient(5em circle at top left, yellow, blue)",
 				"radial-gradient(circle farthest-side at top left, white, black)"*/
 			],
-			"repeating-linear-gradient": "repeating-linear-gradient(white, black)",
-			"repeating-radial-gradient": "repeating-radial-gradient(white, black)",
-			"cross-fade()": [
-				"cross-fade(url(a.png), url(b.png))",
-				"cross-fade(url(a.png) 50%, url(b.png))",
-				"cross-fade(url(a.png) 50%, white)"
-			]
+			"repeating-linear-gradient()": "repeating-linear-gradient(white, black)",
+			"repeating-radial-gradient()": "repeating-radial-gradient(white, black)",
 		},
 		"properties": {
 			"object-fit": ["fill", "contain", "cover", "none", "scale-down"],
@@ -122,13 +117,13 @@ window.Specs = {
 				"cursor",
 				"content"
 			],
-			"linear-gradient": [
+			"linear-gradient()": [
 				"linear-gradient(45deg, #f06 25%, yellow 25% 50%, #f06 50% 75%, yellow 75%)"
 			],
-			"radial-gradient": [
+			"radial-gradient()": [
 				"radial-gradient(center, red 0% 25%, blue 25% 75%, red 75% 100%)"
 			],
-			"conic-gradient": [
+			"conic-gradient()": [
 				"conic-gradient(white, black)",
 				"conic-gradient(from 5deg, white, black)",
 				"conic-gradient(at top left, white, black)",
@@ -138,19 +133,31 @@ window.Specs = {
 				"conic-gradient(currentColor, black)",
 				"conic-gradient(black 25%, white 0deg 50%, black 0deg 75%, white 0deg)"
 			],
-			"repeating-conic-gradient": [
+			"repeating-conic-gradient()": [
 				"repeating-conic-gradient(white, black)",
 				"repeating-conic-gradient(hsla(0, 0%, 100%, .2) 0deg 15deg, hsla(0, 0%, 100%, 0) 0deg 30deg)"
 			],
 			"image()": [
 				"image('sprites.png#xywh=10,30,60,20')",
 				"image('wavy.svg', 'wavy.png' , 'wavy.gif')",
-				"image('dark.png', black)", "image(green)"
+				"image('dark.png', black)",
+				"image(green)"
 			],
-			"element()": "element(#foo)"
+			"image-set()": [
+				"image-set('foo.png' 1x, 'foo-2x.png' 2x, 'foo-print.png' 600dpi)"
+			],
+			"element()": "element(#foo)",
+			"cross-fade()": [
+				"cross-fade(url(a.png), url(b.png))",
+				"cross-fade(url(a.png) 50%, url(b.png))",
+				"cross-fade(url(a.png) 50%, white)"
+			]
 		},
 		"properties": {
-			"image-resolution": ["from-image", "from-image snap", "snap from-image", "1dppx", "1dpcm", "300dpi", "from-image 300dpi", "300dpi from-image", "300dpi from-image snap"]
+			"image-resolution": [
+				"from-image", "from-image snap", "snap from-image",
+				"1dppx", "1dpcm", "300dpi", "from-image 300dpi", "300dpi from-image", "300dpi from-image snap"
+			]
 		}
 	},
 
