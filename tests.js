992acc4178eaba118310e528c7ed26ea061f5948
Zéfling (Célian) <zefling@ikilote.net>
2019-11-03 21:33:17 +0100

    Update subgrid tests for Grid Layout Level 2

--- tests.js
+++ tests.js
@@ -3134,14 +3134,30 @@ window.Specs = {
 					"tr": "#subgrid-per-axis",
 					"dev": "#subgrid-per-axis"
 				},
-				"tests": ["subgrid"]
+				"tests": [
+					"subgrid",
+					"subgrid [sub-a]",
+					"subgrid [sub-a] [sub-b]",
+					"subgrid repeat(1, [sub-a])",
+					"subgrid repeat(2, [sub-a] [sub-b]) [sub-c]",
+					"subgrid repeat(auto-fill, [sub-a] [sub-b])",
+					"subgrid [sub-a]repeat( auto-fill, [sub-b] [sub-c] [sub-d]) [sub-e] repeat(1, [sub-g])"
+				]
 			},
 			"grid-template-rows": {
 				"links": {
 					"tr": "#subgrid-per-axis",
 					"dev": "#subgrid-per-axis"
 				},
-				"tests": ["subgrid"]
+				"tests": [
+					"subgrid",
+					"subgrid [sub-a]",
+					"subgrid [sub-a] [sub-b]",
+					"subgrid repeat(1, [sub-a])",
+					"subgrid repeat(2, [sub-a] [sub-b]) [sub-c]",
+					"subgrid repeat(auto-fill, [sub-a] [sub-b])",
+					"subgrid [sub-a]repeat( auto-fill, [sub-b] [sub-c] [sub-d]) [sub-e] repeat(1, [sub-g])"
+				]
 			}
 		}
 	},
