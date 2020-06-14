a950e99c537b43806b034cec2c93c4d1f2dabf0f
Sebastian Zartner <sebastianzartner@gmail.com>
2020-06-15 01:12:05 +0200

    Added CSS Rhythmic Sizing Level 1

--- tests.js
+++ tests.js
@@ -5503,5 +5503,65 @@ window.Specs = {
 				"tests": ["none", "'M 20 20 H 80 V 30'"]
 			}
 		}
+	},
+
+	"css-rhythmic": {
+		"title": "Rhythmic Sizing",
+		"links": {
+			"tr": "css-rhythm-1",
+			"dev": "css-rhythm"
+		},
+		"properties": {
+			"line-height-step": {
+				"links": {
+					"tr": "#line-height-step",
+					"dev": "#line-height-step"
+				},
+				"tests": ["none", "30px", "2em"]
+			},
+			"block-step-size": {
+				"links": {
+					"tr": "#block-step-size",
+					"dev": "#block-step-size"
+				},
+				"tests": ["none", "30px", "2em"]
+			},
+			"block-step-insert": {
+				"links": {
+					"tr": "#block-step-insert",
+					"dev": "#block-step-insert"
+				},
+				"tests": ["margin", "padding"]
+			},
+			"block-step-align": {
+				"links": {
+					"tr": "#block-step-align",
+					"dev": "#block-step-align"
+				},
+				"tests": ["auto", "center", "start", "end"]
+			},
+			"block-step-round": {
+				"links": {
+					"tr": "#block-step-round",
+					"dev": "#block-step-round"
+				},
+				"tests": ["up", "down", "nearest"]
+			},
+			"block-step": {
+				"links": {
+					"tr": "#block-step",
+					"dev": "#block-step"
+				},
+				"tests": [
+					"none",
+					"padding",
+					"end",
+					"down",
+					"30px margin",
+					"30px padding center",
+					"2em padding start nearest"
+				]
+			}
+		}
 	}
 };
