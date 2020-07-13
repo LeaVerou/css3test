8bd2bca08d5c5f88e03333aa2f45df76e8abe743
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2020-07-14 00:46:13 +0200

    Add SVG 2 Scripting and Interactivity

--- tests.js
+++ tests.js
@@ -5690,6 +5690,24 @@ window.Specs = {
 		}
 	},
 
+	"svg2-interact": {
+		"title": "SVG 2 Scripting and Interactivity",
+		"links": {
+			"tr": "svg2/interact.html",
+			"dev": "svg2-draft/interact.html",
+			"devtype": "svgwg"
+		},
+		"properties": {
+			"pointer-events": {
+				"links": {
+					"tr": "#PointerEventsProp",
+					"dev": "#PointerEventsProp"
+				},
+				"tests": ["auto", "bounding-box", "visiblePainted", "visibleFill", "visibleStroke", "visible", "painted", "fill", "stroke", "all", "none"]
+			}
+		}
+	},
+
 	"css-rhythmic": {
 		"title": "Rhythmic Sizing",
 		"links": {
