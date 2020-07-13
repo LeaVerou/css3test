164f70e20f8b965b7675d398679e007002547b0c
TrisTOON <36267812+TrisTOON@users.noreply.github.com>
2020-07-14 00:43:08 +0200

    Add SVG 2 Text

--- tests.js
+++ tests.js
@@ -5532,6 +5532,68 @@ window.Specs = {
 		}
 	},
 
+	"svg2-text": {
+		"title": "SVG 2 Text",
+		"links": {
+			"tr": "svg2/text.html",
+			"dev": "svg2-draft/text.html",
+			"devtype": "svgwg"
+		},
+		"properties": {
+			"shape-subtract": {
+				"links": {
+					"tr": "#TextShapeSubtract",
+					"dev": "#TextShapeSubtract"
+				},
+				"tests": [
+					"none",
+					"url('#shape')",
+					"inset(50%)",
+					"circle()",
+					"ellipse()",
+					"polygon(0 10px, 30px 0)",
+					"path('M 20 20 H 80 V 30')",
+					"url('#clip') circle()"
+				]
+			},
+			"text-anchor": {
+				"links": {
+					"tr": "#TextAnchoringProperties",
+					"dev": "#TextAnchoringProperties"
+				},
+				"tests": ["start", "middle", "end"]
+			},
+			"text-decoration-fill": {
+				"links": {
+					"tr": "#TextDecorationFillStroke"
+				},
+				"tests": [
+					"none",
+					"green",
+					"url(#pattern)",
+					"url(#pattern) none",
+					"url(#pattern) green",
+					"context-fill",
+					"context-stroke"
+				]
+			},
+			"text-decoration-stroke": {
+				"links": {
+					"tr": "#TextDecorationFillStroke"
+				},
+				"tests": [
+					"none",
+					"green",
+					"url(#pattern)",
+					"url(#pattern) none",
+					"url(#pattern) green",
+					"context-fill",
+					"context-stroke"
+				]
+			}
+		}
+	},
+
 	"css-rhythmic": {
 		"title": "Rhythmic Sizing",
 		"links": {
