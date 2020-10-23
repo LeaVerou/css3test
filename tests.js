072abf039c029651a05f2d332e2e86d24465121e
Zéfling <zefling@ikilote.net>
2020-10-23 22:29:26 +0200

    Add Grid Layout Level 3

--- tests.js
+++ tests.js
@@ -3366,6 +3366,60 @@ window.Specs = {
 		}
 	},
 
+	"css-grid-3": {
+		"title": "CSS Grid Layout Module Level 3",
+		"links": {
+			"dev": "css-grid-3",
+		},
+		"properties": {
+			"grid-template-columns": {
+				"links": {
+					"dev": "#masonry-layout"
+				},
+				"tests": ["masonry"]
+			},
+			"grid-template-rows": {
+				"links": {
+					"dev": "#masonry-layout"
+				},
+				"tests": ["masonry "]
+			},
+			"masonry-auto-flow": {
+				"links": {
+					"dev": "#masonry-auto-flow"
+				},
+				"tests": [
+					"pack", "next", "definite-first", "ordered",
+					"pack definite-first", "pack ordered", "next definite-first", "next ordered",
+					"ordered pack",
+				]
+			},
+			"align-tracks": {
+				"links": {
+					"dev": "#tracks-alignment"
+				},
+				"tests": [
+					"normal",
+					"baseline", "first baseline", "last baseline",
+					"space-between", "space-around", "space-evenly", "stretch",
+					"center", "start", "end", "flex-start", "flex-end",
+					"unsafe center", "safe start"
+				]
+			},
+			"justify-tracks": {
+				"links": {
+					"dev": "#tracks-alignment"
+				},
+				"tests": [
+					"normal",
+					"space-between", "space-around", "space-evenly", "stretch",
+					"center", "start", "end", "flex-start", "flex-end", "left", "right",
+					"unsafe center", "safe start"
+				]
+			}
+		}
+	},
+
 	"css-align-3": {
 		"title": "CSS Box Alignment Module Level 3",
 		"links": {
