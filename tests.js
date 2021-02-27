80dccee2a7c515f7d55d746d245b6ce235cdca7e
Zéfling <zefling@ikilote.net>
2021-02-27 22:09:32 +0100

    Update Box Sizing Level 4

--- tests.js
+++ tests.js
@@ -5316,10 +5316,38 @@ window.Specs = {
 			},
 			"contain-intrinsic-size": {
 				"links": {
+					"tr": "#propdef-contain-intrinsic-size",
+					"dev": "#propdef-contain-intrinsic-size"
+				},
+				"tests": ["none", "10px", "10px 15px"]
+			},
+			"contain-intrinsic-width": {
+				"links": {
 					"tr": "#intrinsic-size-override",
 					"dev": "#intrinsic-size-override"
 				},
-				"tests": ["none", "10px", "10px 15px"]
+				"tests": ["none", "10px"]
+			},
+			"contain-intrinsic-height": {
+				"links": {
+					"tr": "#intrinsic-size-override",
+					"dev": "#intrinsic-size-override"
+				},
+				"tests": ["none", "10px"]
+			},
+			"contain-intrinsic-block-size": {
+				"links": {
+					"tr": "#intrinsic-size-override",
+					"dev": "#intrinsic-size-override"
+				},
+				"tests": ["none", "10px"]
+			},
+			"contain-intrinsic-inline-size": {
+				"links": {
+					"tr": "#intrinsic-size-override",
+					"dev": "#intrinsic-size-override"
+				},
+				"tests": ["none", "10px"]
 			},
 			"width": {
 				"links": {
@@ -5357,6 +5385,48 @@ window.Specs = {
 				"tests": ["stretch", "fit-content", "contain"]
 			},
 			"max-height": {
+				"links": {
+					"tr": "#sizing-values",
+					"dev": "#sizing-values"
+				},
+				"tests": ["stretch", "fit-content", "contain"]
+			},
+			"inline-size": {
+				"links": {
+					"tr": "#sizing-values",
+					"dev": "#sizing-values"
+				},
+				"tests": ["stretch", "fit-content", "contain"]
+			},
+			"min-inline-size": {
+				"links": {
+					"tr": "#sizing-values",
+					"dev": "#sizing-values"
+				},
+				"tests": ["stretch", "fit-content", "contain"]
+			},
+			"max-inline-size": {
+				"links": {
+					"tr": "#sizing-values",
+					"dev": "#sizing-values"
+				},
+				"tests": ["stretch", "fit-content", "contain"]
+			},
+			"block-size": {
+				"links": {
+					"tr": "#sizing-values",
+					"dev": "#sizing-values"
+				},
+				"tests": ["stretch", "fit-content", "contain"]
+			},
+			"min-block-size": {
+				"links": {
+					"tr": "#sizing-values",
+					"dev": "#sizing-values"
+				},
+				"tests": ["stretch", "fit-content", "contain"]
+			},
+			"max-block-size": {
 				"links": {
 					"tr": "#sizing-values",
 					"dev": "#sizing-values"
