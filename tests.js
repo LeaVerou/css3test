3dac5735bd0decb0551c396c1a4dfc52f9b32b2b
Zéfling <zefling@ikilote.net>
2021-08-12 22:45:50 +0200

    Add tests for font-size-adjust (Level 5)

--- tests.js
+++ tests.js
@@ -2619,6 +2619,20 @@ window.Specs = {
 		"links": {
 			"dev": "css-fonts-5"
 		},
+		"properties": {
+			"font-size-adjust": {
+				"links": {
+					"dev": "#font-size-adjust-prop"
+				},
+				"tests": [
+					"ex-height 0.545", "ex-height from-font",
+					"cap-height 0.545", "cap-height from-font",
+					"ch-width 0.545", "ch-width from-font",
+					"ic-width 0.545", "ic-width from-font",
+					"ic-height 0.545", "ic-height from-font"
+				]
+			},
+		},
 		"descriptors": {
 			"@font-face/size-adjust": {
 				"links": {
