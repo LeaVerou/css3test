68aaac20671ff3d3cbf4018d2a901244e5f6e1f6
Zéfling <zefling@ikilote.net>
2021-12-25 21:57:29 +0100

    Update media queries tests

--- tests.js
+++ tests.js
@@ -1283,77 +1283,77 @@ window.Specs = {
 					"tr": "#prefers-reduced-motion",
 					"dev": "#prefers-reduced-motion"
 				},
-				"tests": ["(prefers-reduced-motion: no-preference)", "(prefers-reduced-motion: reduce)"]
+				"tests": ["(prefers-reduced-motion)", "(prefers-reduced-motion: no-preference)", "(prefers-reduced-motion: reduce)"]
 			},
 			"prefers-reduced-transparency": {
 				"links": {
 					"tr": "#prefers-reduced-transparency",
 					"dev": "#prefers-reduced-transparency"
 				},
-				"tests": ["(prefers-reduced-transparency: no-preference)", "(prefers-reduced-transparency: reduce)"]
+				"tests": ["(prefers-reduced-transparency)","(prefers-reduced-transparency: no-preference)", "(prefers-reduced-transparency: reduce)"]
 			},
 			"prefers-contrast": {
 				"links": {
 					"tr": "#prefers-contrast",
 					"dev": "#prefers-contrast"
 				},
-				"tests": ["(prefers-contrast: no-preference)", "(prefers-contrast: high)", "(prefers-contrast: low)", "(prefers-contrast: forced)"]
+				"tests": ["(prefers-contrast)", "(prefers-contrast: no-preference)", "(prefers-contrast: less)", "(prefers-contrast: more)", "(prefers-contrast: custom)"]
 			},
 			"prefers-color-scheme": {
 				"links": {
 					"tr": "#prefers-color-scheme",
 					"dev": "#prefers-color-scheme"
 				},
-				"tests": ["(prefers-color-scheme: light)", "(prefers-color-scheme: dark)"]
+				"tests": ["(prefers-color-scheme)", "(prefers-color-scheme: light)", "(prefers-color-scheme: dark)"]
 			},
 			"scripting": {
 				"links": {
 					"tr": "#scripting",
 					"dev": "#scripting"
 				},
-				"tests": ["(scripting: none)", "(scripting: initial-only)", "(scripting: enabled)"]
+				"tests": ["(scripting)", "(scripting: none)", "(scripting: initial-only)", "(scripting: enabled)"]
 			},
 			"environment-blending": {
 				"links": {
 					"tr": "#environment-blending",
 					"dev": "#environment-blending"
 				},
-				"tests": ["(environment-blending: opaque)", "(environment-blending: additive)", "(environment-blending: subtractive)"]
+				"tests": ["(environment-blending)", "(environment-blending: opaque)", "(environment-blending: additive)", "(environment-blending: subtractive)"]
 			},
 			"forced-colors": {
 				"links": {
 					"tr": "#forced-colors",
 					"dev": "#prefers-contrast"
 				},
-				"tests": ["(forced-colors: none)", "(forced-color: active)"]
+				"tests": ["(forced-colors)", "(forced-colors: none)", "(forced-color: active)"]
 			},
 			"dynamic-range": {
 				"links": {
 					"tr": "#dynamic-range",
 					"dev": "#dynamic-range"
 				},
-				"tests": ["(dynamic-range: standard)", "(dynamic-range: high)"]
+				"tests": ["(dynamic-range)", "(dynamic-range: standard)", "(dynamic-range: high)"]
 			},
 			"inverted-colors": {
 				"links": {
 					"tr": "#inverted",
 					"dev": "#inverted"
 				},
-				"tests": ["(inverted-colors: none)", "(light-level: inverted)"]
+				"tests": ["(inverted-colors)", "(inverted-colors: none)", "(light-level: inverted)"]
 			},
 			"video-color-gamut": {
 				"links": {
 					"dev": "#video-color-gamut",
 					"tr": "#video-color-gamut"
 				},
-				"tests": ["(video-color-gamut: srgb)", "(video-color-gamut: p3)", "(video-color-gamut: rec2020)"]
+				"tests": ["(video-color-gamut)", "(video-color-gamut: srgb)", "(video-color-gamut: p3)", "(video-color-gamut: rec2020)"]
 			},
 			"video-dynamic-range": {
 				"links": {
 					"dev": "#video-dynamic-range",
 					"tr": "#video-dynamic-range"
 				},
-				"tests": ["(video-dynamic-range: standard)", "(video-dynamic-range: high)"]
+				"tests": ["(video-dynamic-range)", "(video-dynamic-range: standard)", "(video-dynamic-range: high)"]
 			}
 		}
 	},
