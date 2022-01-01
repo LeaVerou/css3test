c587331e7555147224441607db3e8b662520a1b1
Sebastian Zartner <sebastianzartner@gmail.com>
2022-01-01 01:08:10 +0100

    Added Animations 2

--- tests.js
+++ tests.js
@@ -224,6 +224,37 @@ window.Specs = {
 		}
 	},
 
+	"css-animations-2": {
+		"title": "CSS Animations Level 2",
+		"links": {
+			"tr": "css-animations-2",
+			"dev": "css-animations-2"
+		},
+		"properties": {
+			"animation-composition": {
+				"links": {
+					"tr": "#animation-composition",
+					"dev": "#animation-composition"
+				},
+				"tests": ["replace", "add", "accumulate", "replace, add, accumulate"]
+			},
+			"animation-timeline": {
+				"links": {
+					"tr": "#animation-timeline",
+					"dev": "#animation-timeline"
+				},
+				"tests": ["auto", "none", "custom-timeline", "\"custom-timeline\"", "auto, none, custom-timeline, \"custom-timeline\""]
+			},
+			"animation": {
+				"links": {
+					"tr": "#animation-timeline",
+					"dev": "#animation-timeline"
+				},
+				"tests": ["1s both infinite auto"]
+			}
+		}
+	},
+
 	"css-backgrounds-3": {
 		"title": "CSS Backgrounds and Borders Module Level 3",
 		"links": {
