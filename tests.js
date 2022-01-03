f1cdd8ade02706819678fae7956f7e3fb6f8ff7c
Sebastian Zartner <sebastianzartner@gmail.com>
2022-01-03 21:14:17 +0100

    Added Spatial Navigation 1

--- tests.js
+++ tests.js
@@ -4451,6 +4451,37 @@ window.Specs = {
 		}
 	},
 
+	"css-nav-1": {
+		"title": "CSS Spatial Navigation Level 1",
+		"links": {
+			"tr": "css-nav-1",
+			"dev": "css-nav-1"
+		},
+		"properties": {
+			"spatial-navigation-action": {
+				"links": {
+					"tr": "#css-property-spatialnavigationaction",
+					"dev": "#css-property-spatialnavigationaction"
+				},
+				"tests": ["auto", "focus", "scroll"]
+			},
+			"spatial-navigation-contain": {
+				"links": {
+					"tr": "#container",
+					"dev": "#container"
+				},
+				"tests": ["auto", "contain"]
+			},
+			"spatial-navigation-function": {
+				"links": {
+					"tr": "#css-property-spatialnavigationfunction",
+					"dev": "#css-property-spatialnavigationfunction"
+				},
+				"tests": ["normal", "grid"]
+			}
+		}
+	},
+
 	"css-text-3": {
 		"title": "CSS Text Module Level 3",
 		"links": {
