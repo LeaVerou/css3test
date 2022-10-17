1ab4ed9b28197ae4eba901c1cc564669db5a83ee
Sebastian Zartner <sebastianzartner@gmail.com>
2022-10-17 23:44:45 +0200

    Added test for length in fit-content() for CSS Box Sizing 3

--- tests/css-sizing-3.js
+++ tests/css-sizing-3.js
@@ -13,49 +13,84 @@ export default {
 				tr: '#preferred-size-properties',
 				dev: '#preferred-size-properties',
 			},
-			tests: ['max-content', 'min-content', 'fit-content(10%)'],
+			tests: [
+				'max-content',
+				'min-content',
+				'fit-content(200px)',
+				'fit-content(10%)',
+			],
 		},
 		'min-width': {
 			links: {
 				tr: '#min-size-properties',
 				dev: '#min-size-properties',
 			},
-			tests: ['max-content', 'min-content', 'fit-content(10%)'],
+			tests: [
+				'max-content',
+				'min-content',
+				'fit-content(200px)',
+				'fit-content(10%)',
+			],
 		},
 		'max-width': {
 			links: {
 				tr: '#max-size-properties',
 				dev: '#max-size-properties',
 			},
-			tests: ['max-content', 'min-content', 'fit-content(10%)'],
+			tests: [
+				'max-content',
+				'min-content',
+				'fit-content(200px)',
+				'fit-content(10%)',
+			],
 		},
 		height: {
 			links: {
 				tr: '#preferred-size-properties',
 				dev: '#preferred-size-properties',
 			},
-			tests: ['max-content', 'min-content', 'fit-content(10%)'],
+			tests: [
+				'max-content',
+				'min-content',
+				'fit-content(200px)',
+				'fit-content(10%)',
+			],
 		},
 		'min-height': {
 			links: {
 				tr: '#min-size-properties',
 				dev: '#min-size-properties',
 			},
-			tests: ['max-content', 'min-content', 'fit-content(10%)'],
+			tests: [
+				'max-content',
+				'min-content',
+				'fit-content(200px)',
+				'fit-content(10%)',
+			],
 		},
 		'max-height': {
 			links: {
 				tr: '#max-size-properties',
 				dev: '#max-size-properties',
 			},
-			tests: ['max-content', 'min-content', 'fit-content(10%)'],
+			tests: [
+				'max-content',
+				'min-content',
+				'fit-content(200px)',
+				'fit-content(10%)',
+			],
 		},
 		'column-width': {
 			links: {
 				tr: '#column-sizing',
 				dev: '#column-sizing',
 			},
-			tests: ['max-content', 'min-content', 'fit-content(10%)'],
+			tests: [
+				'max-content',
+				'min-content',
+				'fit-content(200px)',
+				'fit-content(10%)',
+			],
 		},
 	},
 };
