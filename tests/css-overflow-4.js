f3ed112d7a9305f968ea5a349e7b5d2c68440039
Sebastian Zartner <sebastianzartner@gmail.com>
2023-08-04 22:32:41 +0200

    Added test for over

--- tests/css-overflow-4.js
+++ tests/css-overflow-4.js
@@ -7,6 +7,148 @@ export default {
 	status: {
 		stability: 'experimental',
 	},
+	properties: {
+		'overflow-clip-margin-top': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-right': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-bottom': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-left': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-block-start': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-inline-start': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-block-end': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-inline-end': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-block': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+		'overflow-clip-margin-inline': {
+			links: {
+				tr: '#overflow-clip-margin',
+				dev: '#overflow-clip-margin',
+			},
+			tests: [
+				'content-box',
+				'padding-box',
+				'border-box',
+				'20px',
+				'content-box 20px',
+				'20px content-box',
+			],
+		},
+	},
 	selectors: {
 		'::nth-fragment()': {
 			links: {
