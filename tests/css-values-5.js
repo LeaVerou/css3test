dd4496e9944354e5d50e75e12a6997923f0482fb
Sebastian Zartner <sebastianzartner@gmail.com>
2023-08-04 21:57:18 +0200

    Updated CSS Values 5

--- tests/css-values-5.js
+++ tests/css-values-5.js
@@ -29,6 +29,63 @@ export default {
 				'attr(data-flex flex)',
 			],
 		},
+		'random()': {
+			links: {
+				dev: '#random',
+			},
+			tests: [
+				'random(0px, 100px)',
+				'random(50%, 100%)',
+				'random(180deg, 360deg)',
+				'random(1s, 10s)',
+				'random(40Hz, 100Hz)',
+				'random(10, 20)',
+				'random(10px, 10em)',
+				'random(-infinity, infinity)',
+				'random(--random-value, 0px, 100px)',
+				'random(per-element, 0px, 100px)',
+				'random(50px, 100%, by 1em)'
+			],
+		},
+		'random-item() with lengths': {
+			links: {
+				dev: '#random-item',
+			},
+			tests: [
+				'random-item(--random-item; 100px)',
+				'random-item(--random-item; 0px; 50px; 100px)',
+				'random-item(per-element; 0px; 50px; 100px)',
+				'random-item(--random-item; 50%; 100%)',
+				'random-item(--random-item; 100px; 10em; 50%; 100%)',
+			],
+		},
+		'random-item() with keywords': {
+			links: {
+				dev: '#random-item',
+			},
+			properties: ['color'],
+			tests: [
+				'random-item(--random-item; red; yellow; green; blue)',
+			],
+		},
+		'random-item() with keywords': {
+			links: {
+				dev: '#random-item',
+			},
+			properties: ['color'],
+			tests: [
+				'random-item(--random-item; red; yellow; green; blue)',
+			],
+		},
+		'random-item() with functions': {
+			links: {
+				dev: '#random-item',
+			},
+			properties: ['background-image'],
+			tests: [
+				'random-item(--random-item; linear-gradient(red, yellow); linear-gradient(green, blue))',
+			],
+		},
 		'toggle() with lengths': {
 			links: {
 				dev: '#toggle-notation',
@@ -48,6 +105,26 @@ export default {
 			},
 			properties: ['background-position'],
 			tests: ['toggle(top left, 100% 50%)'],
+		},
+		'Request URL modifiers': {
+			links: {
+				dev: '#request-url-modifiers',
+			},
+			tests: [
+				'url("image.png" crossorigin(anonymous))',
+				'url("image.png" crossorigin(use-credentials))',
+				'url("image.png" integrity("8f6846e1bad406933f9122b201c6de07"))',
+				'url("image.png" referrerpolicy(no-referrer))',
+				'url("image.png" referrerpolicy(no-referrer-when-downgrade))',
+				'url("image.png" referrerpolicy(same-origin))',
+				'url("image.png" referrerpolicy(origin))',
+				'url("image.png" referrerpolicy(strict-origin))',
+				'url("image.png" referrerpolicy(origin-when-cross-origin))',
+				'url("image.png" referrerpolicy(strict-origin-when-cross-origin))',
+				'url("image.png" referrerpolicy(unsafe-url))',
+				'url("image.png" crossorigin(anonymous) referrerpolicy(no-referrer))',
+				'url("image.png" crossorigin(anonymous) integrity("8f6846e1bad406933f9122b201c6de07") referrerpolicy(no-referrer))',
+			],
 		},
 	},
 };
