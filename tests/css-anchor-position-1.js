a22f2879a2fdc9d9f66f8fe477fc2d3f849f40be
Sebastian Zartner <sebastianzartner@gmail.com>
2024-01-02 23:54:58 +0100

    Updated CSS Anchor Position 1

--- tests/css-anchor-position-1.js
+++ tests/css-anchor-position-1.js
@@ -27,8 +27,8 @@ export default {
 				'inset',
 			],
 			tests: [
-				'anchor(auto)',
-				'anchor(auto-same)',
+				'anchor(inside)',
+				'anchor(outside)',
 				'anchor(top)',
 				'anchor(left)',
 				'anchor(right)',
@@ -39,8 +39,8 @@ export default {
 				'anchor(self-end)',
 				'anchor(center)',
 				'anchor(30%)',
-				'anchor(auto, 20px)',
-				'anchor(auto-same, 20px)',
+				'anchor(inside, 20px)',
+				'anchor(outside, 20px)',
 				'anchor(top, 20px)',
 				'anchor(start, 20px)',
 				'anchor(30%, 20px)',
@@ -52,10 +52,24 @@ export default {
 				'anchor(implicit top, 5%)',
 			],
 		},
+		'anchor-center': {
+			links: {
+				dev: '#anchor-center',
+			},
+			properties: [
+				'justify-self',
+				'align-self',
+				'justify-items',
+				'align-items',
+			],
+			tests: [
+				'anchor-center',
+			],
+		},
 		'anchor-size()': {
 			links: {
-				tr: '#anchor-pos',
-				dev: '#anchor-pos',
+				tr: '#anchor-size-fn',
+				dev: '#anchor-size-fn',
 			},
 			properties: [
 				'width',
@@ -91,17 +105,54 @@ export default {
 		},
 		'anchor-name': {
 			links: {
-				tr: '#determining',
-				dev: '#determining',
+				tr: '#name',
+				dev: '#name',
 			},
 			tests: ['none', '--anchor'],
 		},
-		'anchor-scroll': {
+		'inset-area': {
 			links: {
-				tr: '#scroll',
-				dev: '#scroll',
+				dev: '#inset-area',
 			},
-			tests: ['none', 'default', '--anchor', 'implicit'],
+			tests: [
+				'none',
+				'start',
+				'end',
+				'center',
+				'self-start',
+				'self-end',
+				'top',
+				'bottom',
+				'left',
+				'right',
+				'x-start',
+				'x-end',
+				'y-start',
+				'y-end',
+				'x-self-start',
+				'x-self-end',
+				'y-self-start',
+				'y-self-end',
+				'all',
+				'start end',
+				'start center end',
+				'self-start self-end',
+				'self-start center self-end',
+				'top bottom',
+				'top center bottom',
+				'left right',
+				'left center right',
+				'x-start x-end',
+				'x-start center x-end',
+				'y-start y-end',
+				'y-start center y-end',
+				'x-self-start x-self-end',
+				'x-self-start center x-self-end',
+				'y-self-start y-self-end',
+				'y-self-start center y-self-end',
+				'left / bottom',
+				'x-start / y-end',
+			],
 		},
 		'position-fallback': {
 			links: {
@@ -109,6 +160,13 @@ export default {
 				dev: '#fallback-property',
 			},
 			tests: ['none', '--fallback'],
+		},
+		'position-fallback-bounds': {
+			links: {
+				tr: '#fallback-bounds',
+				dev: '#fallback-bounds',
+			},
+			tests: ['normal', '--fallback'],
 		},
 	},
 	'@rules': {
@@ -118,6 +176,30 @@ export default {
 				dev: '#fallback-rule',
 			},
 			tests: "@position-fallback --button-popover {\n  @try {\n    top: anchor(--button bottom);\n    left: anchor(--button left);\n  }\n\n  @try {\n    bottom: anchor(--button top);\n    right: anchor(--button right);\n  }\n}",
+		},
+	},
+	interfaces: {
+		CSSPositionFallbackRule: {
+			links: {
+				dev: '#position-fallback-rule',
+				mdnGroup: 'DOM',
+			},
+			tests: ['name', 'cssRules', 'insertRule', 'deleteRule'],
+			required: '@position-fallback --button-popover { @try { top: anchor(--button bottom); left: anchor(--button left); } }',
+			interface: function(style) {
+				return style.sheet.cssRules[0];
+			}
+		},
+		CSSTryRule: {
+			links: {
+				dev: '#position-try-rule',
+				mdnGroup: 'DOM',
+			},
+			tests: ['style', 'cssText', 'parentRule', 'parentStyleSheet'],
+			required: '@position-fallback --button-popover { @try { top: anchor(--button bottom); left: anchor(--button left); } }',
+			interface: function(style) {
+				return style.sheet.cssRules[0].cssRules[0];
+			}
 		},
 	},
 };
