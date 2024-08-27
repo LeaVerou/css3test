962f22c6650f9db2aa3f30442fa93d47e18b5385
Sebastian Zartner <sebastianzartner@gmail.com>
2024-08-27 22:48:29 +0200

    Updated CSS Inline 3

--- tests/css-inline-3.js
+++ tests/css-inline-3.js
@@ -29,14 +29,26 @@ export default {
 				dev: '#baseline-shift-property',
 				tr: '#baseline-shift-property',
 			},
-			tests: ['5px', '10%', 'sub', 'super', 'top', 'center', 'bottom'],
+			tests: [
+				'5px',
+				'10%',
+				'sub',
+				'super',
+				'top',
+				'center',
+				'bottom',
+			],
 		},
 		'baseline-source': {
 			links: {
 				dev: '#baseline-source',
 				tr: '#baseline-source',
 			},
-			tests: ['auto', 'first', 'last'],
+			tests: [
+				'auto',
+				'first',
+				'last',
+			],
 		},
 		'dominant-baseline': {
 			links: {
@@ -60,37 +72,53 @@ export default {
 				dev: '#sizing-drop-initials',
 				tr: '#sizing-drop-initials',
 			},
-			tests: ['normal', '1.4 3', '1.4', '1.4 drop', '1.4 raise'],
+			tests: [
+				'normal',
+				'1.4 3',
+				'1.4',
+				'1.4 drop',
+				'1.4 raise',
+			],
 		},
 		'initial-letter-align': {
 			links: {
 				dev: '#aligning-initial-letter',
 				tr: '#aligning-initial-letter',
 			},
-			tests: ['border-box', 'alphabetic', 'ideographic', 'hanging', 'leading', 'border-box alphabetic'],
+			tests: [
+				'border-box',
+				'alphabetic',
+				'ideographic',
+				'hanging',
+				'leading',
+				'border-box alphabetic'
+			],
 		},
 		'initial-letter-wrap': {
 			links: {
 				dev: '#initial-letter-wrapping',
 				tr: '#initial-letter-wrapping',
 			},
-			tests: ['none', 'first', 'all', 'grid', '30px', '10%'],
+			tests: [
+				'none',
+				'first',
+				'all',
+				'grid',
+				'30px',
+				'10%',
+			],
 		},
 		'inline-sizing': {
 			links: {
 				dev: '#line-fill',
 				tr: '#line-fill',
 			},
-			tests: ['normal', 'stretch'],
+			tests: [
+				'normal',
+				'stretch',
+			],
 		},
-		'leading-trim': {
-			links: {
-				dev: '#leading-trim',
-				tr: '#leading-trim',
-			},
-			tests: ['normal', 'start', 'end', 'both'],
-		},
-		'text-edge': {
+		'line-fit-edge': {
 			links: {
 				dev: '#text-edges',
 				tr: '#text-edges',
@@ -102,16 +130,80 @@ export default {
 				'ex',
 				'ideographic',
 				'ideographic-ink',
+				'text text',
 				'text alphabetic',
-				'cap ideographic-ink',
+				'cap ideographic',
+				'ideographic ideographic-ink',
 			],
+		},
+		'text-box': {
+			links: {
+				dev: '#text-box-shorthand',
+				tr: '#text-box-shorthand',
+			},
+			tests: [
+				'normal',
+				'none',
+				'trim-start',
+				'trim-end',
+				'trim-both',
+				'auto',
+				'text',
+				'cap',
+				'ex',
+				'ideographic',
+				'ideographic-ink',
+				'text text',
+				'text alphabetic',
+				'cap ideographic',
+				'ideographic ideographic-ink',
+				'trim-start text',
+				'trim-end cap',
+				'trim-both ex',
+			],
+		},
+		'text-box-edge': {
+			links: {
+				dev: '#text-box-edge',
+				tr: '#text-box-edge',
+			},
+			tests: [
+				'auto',
+				'text',
+				'cap',
+				'ex',
+				'ideographic',
+				'ideographic-ink',
+				'text text',
+				'text alphabetic',
+				'cap ideographic',
+				'ideographic ideographic-ink',
+			],
+		},
+		'text-box-trim': {
+			links: {
+				dev: '#text-box-trim',
+				tr: '#text-box-trim',
+			},
+			tests: ['none', 'trim-start', 'trim-end', 'trim-both'],
 		},
 		'vertical-align': {
 			links: {
 				dev: '#transverse-alignment',
 				tr: '#transverse-alignment',
 			},
-			tests: ['first', 'last', 'first text-bottom', 'last sub', 'super text-bottom first'],
+			tests: [
+				'first',
+				'last',
+				'alphabetic',
+				'ideographic',
+				'central',
+				'mathematical',
+				'center',
+				'first text-bottom',
+				'last sub',
+				'super text-bottom first'
+			],
 		},
 	},
 };
