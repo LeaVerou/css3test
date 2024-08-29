a5813874123a414311f27a515d05e6442442b441
Zéfling <zefling@ikilote.net>
2024-08-29 19:59:14 +0200

    Added color-layers()

--- tests/css-color-6.js
+++ tests/css-color-6.js
@@ -18,5 +18,20 @@ export default {
 				'color-contrast(hsl(200 50% 80%) vs hsl(200 83% 23%), purple, hsl(300 100% 25%))',
 			],
 		},
+		'color-layers()': {
+			links: {
+				dev: '#color-layers',
+				mdn: 'color_value/color-layers',
+			},
+			tests: [
+				'color-layers(rgb(from yellow r g b / 0.5))',
+				'color-layers(rgb(from yellow r g b / 0.5), red)',
+				'color-layers(rgb(from yellow r g b / 0.5), color(display-p3 1 0.5 0))',
+				'color-layers(rgb(from yellow r g b / 0.5), color-mix(in lab, red, blue))',
+				'color-layers(rgb(from yellow r g b / 0.5), rgba(0, 255, 128, 0.6), currentcolor, hsla(0, 100%, 20%, 0.4))',
+				'color-layers(rgb(from yellow r g b / 0.5), rgba(0, 255, 128, 0.6), color-mix(in lab, purple, rgba(0,0,0,0.5)), color(display-p3 1 0.5 0))',
+				'color-layers(color(display-p3 1 0.5 0), rgb(from yellow r g b / 0.5), rgba(0, 255, 128, 0.6), color-mix(in lab, purple, rgba(0,0,0,0.5)), hsla(0, 100%, 20%, 0.4))'
+			],
+		},
 	},
 };
